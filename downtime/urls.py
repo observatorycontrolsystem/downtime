d070f067c2024ae542c9046ae0683dd4c62a644e
Matt Daily <mdaily@lco.global>
2021-06-07 12:34:28 -0700

    Add OpenAPI schema path with basic autogenerated schema.
    Add some required packages for OpenAPI support to requirements.txt

--- downtime/urls.py
+++ downtime/urls.py
@@ -14,17 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.urls import path
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from schedule.views import DowntimeListView
 from schedule.viewsets import DowntimeViewSet
-
+from rest_framework.schemas import get_schema_view
 
 router = DefaultRouter()
 router.register(r'', DowntimeViewSet, 'downtime')
+
+schema_view = get_schema_view(
+    title="Downtime API",
+    description="An application with a database that stores periods of scheduled telescope downtime for an observatory with an API to access those downtimes",
+    version='2.3.2')
 
 urlpatterns = [
     url(r'^$', DowntimeListView.as_view(), name='web-downtime-list'),
     url(r'^api/', include(router.urls)),
     url(r'^admin/', admin.site.urls),
+    path('openapi/', schema_view, name='openapi-schema'),
 ]
