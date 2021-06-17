a0a9a9c4537336195bb249a6b2b34f8e064bcf86
Matt Daily <mdaily@lco.global>
2021-06-17 10:25:14 -0700

    Update github action for doc generation.
    Point new doc builds to OCS Github pages repository. Only build docs on push to main and tag.

--- downtime/schema.py
+++ downtime/schema.py
@@ -3,8 +3,7 @@ from setuptools_scm import get_version
 from setuptools_scm.version import ScmVersion
 
 def version_scheme(version: ScmVersion) -> str:
-    """
-    Simply return the string representation of the version object tag, which is the latest git tag.
+    """ Simply return the string representation of the version object tag, which is the latest git tag.
 
     setuptools_scm does not provide a simple semantic versioning format without trying to guess the next release, or adding some metadata to the version.
     """
@@ -19,4 +18,3 @@ class DowntimeSchemaGenerator(SchemaGenerator):
         # Set the version to the latest git tag, and do not append any local information to the version number
         schema['info']['version'] = get_version(version_scheme=version_scheme, local_scheme='no-local-version')
         return schema
-
